50fedd45633348e5bd4d53ec1a014e1f46b6bfc2
ochprince <879691172@qq.com>
2020-02-16 21:50:12 +0800

    init

---
+++ go.mod
@@ -0,0 +1,31 @@
+module github.com/kubernetes/dashboard
+
+go 1.12
+
+require (
+	github.com/docker/distribution v2.7.0+incompatible
+	github.com/docker/spdystream v0.0.0-20181023171402-6480d4af844c // indirect
+	github.com/elazarl/goproxy v0.0.0-20190421051319-9d40249d3c2f // indirect
+	github.com/elazarl/goproxy/ext v0.0.0-20190421051319-9d40249d3c2f // indirect
+	github.com/emicklei/go-restful v2.9.6+incompatible
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/googleapis/gnostic v0.2.0 // indirect
+	github.com/igm/sockjs-go v2.0.1+incompatible // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/prometheus/client_golang v0.9.2
+	github.com/prometheus/common v0.0.0-20181218105931-67670fe90761 // indirect
+	github.com/prometheus/procfs v0.0.0-20190102135031-14fa7590c24d // indirect
+	github.com/spf13/pflag v1.0.5
+	golang.org/x/net v0.0.0-20191004110552-13f9640d40b9
+	golang.org/x/text v0.3.2
+	gopkg.in/igm/sockjs-go.v2 v2.0.0
+	gopkg.in/square/go-jose.v2 v2.2.2
+	gopkg.in/yaml.v2 v2.2.4
+	k8s.io/api v0.17.2
+	k8s.io/apiextensions-apiserver v0.0.0-20190918161926-8f644eb6e783
+	k8s.io/apimachinery v0.17.2
+	k8s.io/client-go v0.17.2
+	k8s.io/code-generator v0.17.2 // indirect
+	k8s.io/heapster v1.5.4
+)
---
+++ src/app/frontend/index.module.ts
@@ -0,0 +1,48 @@
+// Copyright 2017 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {HttpClientModule} from '@angular/common/http';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {BrowserModule, HAMMER_GESTURE_CONFIG} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import {ChromeModule} from './chrome/module';
+import {CoreModule} from './core.module';
+import {GlobalErrorHandler} from './error/handler';
+import {RootComponent} from './index.component';
+import {routes} from './index.routing';
+import {LoginModule} from './login/module';
+import {GestureConfig} from '@angular/material/core';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule,
+    ChromeModule,
+    LoginModule,
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      onSameUrlNavigation: 'reload',
+    }),
+  ],
+  providers: [
+    {provide: ErrorHandler, useClass: GlobalErrorHandler},
+    {provide: HAMMER_GESTURE_CONFIG, useClass: GestureConfig},
+  ],
+  declarations: [RootComponent],
+  bootstrap: [RootComponent],
+})
+export class RootModule {}
